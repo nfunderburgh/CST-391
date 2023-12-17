cae764c4a2e990f1068c69953e80190c784d8ccc
nfunderburgh <thunderquack205@gmail.com>
2023-12-16 17:13:06 -0700

    Activity 6 finished

--- Activity-6/music/src/App.js
+++ Activity-6/music/src/App.js
@@ -1,47 +1,81 @@
 import React, { useState, useEffect } from "react";
-import Card from './Card.js';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SearchAlbum from "./SearchAlbum.js";
+import NavBar from "./NavBar.js";
+import NewAlbum from "./NewAlbum.js";
+import OneAlbum from "./OneAlbum.js";
 import './App.css';
-import albums from './albums.json';
-import SearchForm from './SearchForm.js';
+import dataSource from "./dataSource.js";
+
 
 const App = (props) => {
     const [searchPhrase, setSearchPhrase] = useState('');
     const [albumList, setAlbumList] = useState([]);
+    const [currentlySelectedAlbumId, setCurrentlySelectedAlbumId] = useState(0);
+
+    let refresh = false;
+
+    const loadAlbums = async () => {
+        const response = await dataSource.get('/albums');
+        setAlbumList(response.data);
+    }
+
+    useEffect(() => {
+        loadAlbums();
+    }, [refresh]);
 
     const updateSearchResults = (phrase) => {
         console.log('phrase is ' + phrase);
         setSearchPhrase(phrase);
-    }
-
-    useEffect(() => {
-        setAlbumList(albums);
-    }, [albumList]);
-
-    const renderedList = () => {
-        return albumList.map((album) => {
-            if(
-                album.description.toLowerCase().includes(searchPhrase.toLowerCase()) ||
-                searchPhrase === ''
-            )
-                return (
-                    <Card 
-                        key={album.id}
-                        albumTitle={album.title} 
-                        albumDescription={album.description} 
-                        buttonText='OK' imageURL={album.image} 
-                    />
-                );
-            else console.log('Does not match ' + searchPhrase);
-        });
     };
-    return(
-        <div>
-            <div className="container">
-                <SearchForm onSubmit={updateSearchResults}/>
-            </div>
-            <div className="container">{renderedList()}</div>
-        </div>
+
+    const updateSingleAlbum = (id, navigate) => {
+        console.log('Update Single album = ', id);
+        console.log('Update Single album = ', navigate);
+        var indexNumber = 0;
+        for (var i = 0; i < albumList.length; ++i) {
+            if (albumList[i].id === id) indexNumber = i;
+        }
+        setCurrentlySelectedAlbumId(indexNumber);
+        console.log('update path', '/show/' + indexNumber);
+        navigate('/show/' + indexNumber);
+    };
+
+    console.log('albumList', albumList);
+    const renderedList = albumList.filter((album) => {
+        if (
+            album.description.toLowerCase().includes(searchPhrase.toLowerCase()) || searchPhrase === ''
+        ) {
+            return true;
+        }
+        return false;
+    });
+    console.log('renderedList', renderedList)
+
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Routes>
+                <Route
+                    exact
+                    path='/'
+                    element={
+                        <SearchAlbum
+                            updateSearchResults={updateSearchResults}
+                            albumList={renderedList}
+                            updateSingleAlbum={updateSingleAlbum}
+                        />
+                    }
+                />
+                <Route exact path='/new' element={<NewAlbum />} />
+                <Route
+                    exact
+                    path='/show/:albumId'
+                    element={<OneAlbum album={albumList[currentlySelectedAlbumId]} />}
+                />
+            </Routes>
+        </BrowserRouter>
     );
-};
+}
 
 export default App;
